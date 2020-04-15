44c030f043f2f5bc80c1f25908e816864ce71bd4
Anbionchik <Anbion@yandex.ru>
2020-04-15 21:17:32 +0300

    Коммит второго занятия

--- main/views.py
+++ main/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 
 
-# Create your views here.
+data = {
+    'lists': [
+        {'name': 'Работа', 'is_done': True},
+        {'name': 'Дом', 'is_done': False},
+        {'name': 'Учёбы', 'is_done': True}
+    ],
+    'user_name' : 'Admin',
+}
+# for list in data['lists']:
+#     name = list[0]
+#     is_done = list[1]
 
 def main_view(request):
-    return render(request, 'index.html')
+    context = data
+    return render(request, 'index.html', context)
